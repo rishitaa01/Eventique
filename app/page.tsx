79cbd621396b1d6afe6510c49c9d65c2389b9176
rishitaa01 <rishitaa0102@gmail.com>
2025-08-31 02:58:36 +0530

    final1

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
             }}
           />
         </div>
-        <div className='mx-auto max-w-2xl py-32 sm:py-48 lg:py-56'>
+        <div className="mx-auto max-w-2xl py-12 sm:py-20 lg:py-28">
           <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
             <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
               Wish to know how to host a great event?{' '}
