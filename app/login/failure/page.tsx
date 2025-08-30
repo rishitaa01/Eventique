0073e8735dd3806f90d1ad7b90e0a068132abb5a
rishitaa01 <rishitaa0102@gmail.com>
2025-08-30 17:33:01 +0530

    Fix dynamic import issues and mark client components

--- app/login/failure/page.tsx
+++ app/login/failure/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 
 const LoginFailureClient = dynamic(() => import("./LoginFailureClient"), {
--- app/login/sucess/page.tsx
+++ app/login/sucess/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 
 const LoginSuccessClient = dynamic(() => import("./LoginSuccessClient"), {
