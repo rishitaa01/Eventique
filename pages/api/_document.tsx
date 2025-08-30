17d0170f034487b8c90e5d8eabfc7a9852e9e45f
rishitaa01 <rishitaa0102@gmail.com>
2025-08-30 22:20:47 +0530

    image thingy

---
+++ pages/api/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          {/* You can add a PNG or SVG favicon too */}
+          <link rel="icon" type="image/png" href="/logo/logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
