d49b0c54c14bae9c2969d4ff7b126cecb33ce66d
rishitaa01 <rishitaa0102@gmail.com>
2025-08-31 16:32:10 +0530

    navibar

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         </nav>
       </header>
 
-      <div className='relative isolate px-6 pt-14 lg:px-8'>
+      <div className="relative isolate px-6 pt-2 lg:px-8">
         <div
           className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
           aria-hidden='true'
